9d8991438921ff94a66d670848481950f8770910
babideer <55578454+babideer@users.noreply.github.com>
2020-12-14 19:16:43 +0100

    Update register.php
    Update for js validering

--- web/register.php
+++ web/register.php
@@ -108,26 +108,24 @@
             </form> 
 
     <!--JS validator-->
-    <script>
+   <script>
             (function() {
-            'use strict';
             window.addEventListener('load', function() {
                 // Fetch all the forms we want to apply custom Bootstrap validation styles to
                 var forms = document.getElementsByClassName('needs-validation');
-                // Loop over them and prevent submission
-                var validation = Array.prototype.filter.call(forms, function(form) {
+
+                //adder eventlistener til vores form
                 form.addEventListener('submit', function(event) {
+                    //tjekker om noget er indtastet
                     if (form.checkValidity() === false) {  
-                    event.preventDefault(); //main event is cancelled -> stops submit
-                    event.stopPropagation(); //prevent bubbling -> takes the inner object
+                    //stopper main event  
+                    //forhindre submit knappen i at lægge data ind i database
+                    event.preventDefault();
                     }
-                    form.classList.add('was-validated');
-                }, false);
                 });
-            }, false);
+            });
             })();
         </script>
-
     <!--JS Bootstrap, Jquery & Popper-->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
